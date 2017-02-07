88a87dfd1e03d027de95fdbd390549b8d5f44879
xucw <xucw@fenqi.im>
2017-02-07 10:22:40 +0800

    add files

---
+++ sub1-test/src/main/java/org/sub1/test/App.java
@@ -0,0 +1,13 @@
+package org.sub1.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
